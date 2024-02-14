a1a369315c6ee7235b17fe62f384721f84422ee5
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-02-14 20:47:19 +0300

    Добавьте файлы проекта.

---
+++ CppEnumProject/CppEnumProject.cpp
@@ -0,0 +1,59 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+enum WayType
+{
+    Left,
+    Right,
+    Up,
+    Down,
+};
+
+enum SideType
+{
+    a,
+    b,
+    c,
+    d
+};
+
+enum class Colors
+{
+    Red,
+    Blue,
+    Green,
+    Yellow
+};
+
+void Go(WayType wayGo)
+{
+    char* str = new char[10];
+    switch (wayGo)
+    {
+    case Left: strcpy(str, "west");
+        break;
+    case Right:strcpy(str, "ost");
+        break;
+    case Up: strcpy(str, "north");
+        break;
+    case Down: strcpy(str, "south");
+        break;
+    default:
+        break;
+    }
+
+    std::cout << "You go to " << str << "\n";
+}
+
+int main()
+{
+    WayType way;
+    way = (WayType)2;
+    way = Down;
+    way = WayType::Left;
+
+    Go(way);
+
+    Colors color{ Colors::Red };
+
+}
